48a159b7da269db9a5653f3febc1020c2a98cb3d
Vorachat Somsuay <vorachat1239@gmail.com>
2022-02-19 20:34:13 +0700

    Decode usage example

---
+++ telescopetc/main.c
@@ -0,0 +1,24 @@
+#include <assert.h>
+#include <inttypes.h>
+#include <stdio.h>
+#include <unistd.h>
+#include <time.h>
+
+#include "wrapper.h"
+
+int main()
+{
+    /** Code here **/
+    struct mstruct m;
+
+    ReadImg("test_compressed.pvr", &m);
+    float st = (float)clock()/CLOCKS_PER_SEC;
+    // *src buffer: m.src_buf | dst buffer: m.dst_buf
+    DecodeDXT5(&m); 
+    float et = (float)clock()/CLOCKS_PER_SEC;
+    float dt = et - st;
+    printf("Decoding Time : %f\n", dt);
+    RenderImg("ourDecode.png", m.m_size.x, m.m_size.y, m.dst_buf);
+
+    return 0;
+}
